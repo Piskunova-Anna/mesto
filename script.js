52fbd4ece2a818ac3b95aeb24a77f34a36c9661e
Piskunova Anna <a777nna102008@yandex.ru>
2021-04-22 00:44:55 +0300

    1 ревью

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let profile__info_button = document.querySelector('.profile__info_button');
-let popup__container = document.querySelector('.popup__container');
+let profile__button = document.querySelector('.profile__button');
+let popup = document.querySelector('.popup');
 let popup__overlay = document.querySelector('.popup__overlay');
 let popup__closeIcon = document.querySelector('.popup__close-icon');
-profile__info_button.addEventListener('click', function() {
+profile__button.addEventListener('click', function() {
     popup__overlay.style.display = 'block';
-    popup__container.style.display = 'flex';
+    popup.style.display = 'flex';
 });
 popup__closeIcon.addEventListener('click', function() {
     popup__overlay.style.display = 'none';
-    popup__container.style.display = 'none';
+    popup.style.display = 'none';
 })
